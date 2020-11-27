5a68116d0f1a06d2c353270f06ffbd5fe6c1c6de
klwieland <73842843+klwieland@users.noreply.github.com>
2020-11-27 14:16:17 -0500

    Preliminary work for Center Block

--- CenterBlock.lua
+++ CenterBlock.lua
@@ -1,5 +1,24 @@
 CenterBlock = Class{}
 
-function CenterBlock:init(util, centerBlockTable, tetriminoTable)
 
+function CenterBlock:init(x,y,height,width)
+    self.x = x
+    self.y = y
+    self.height = height
+    self.width = height
+    self.dy = 0
+    self.dx = 0
 end
+
+function CenterBlock:update(dt)
+    self.y = self.y + self.dy * dt
+    self.x = self.x + self.dx * dt
+end
+
+function CenterBlock:rotate()
+
+end 
+
+function CenterBlock:render()
+    love.graphics.square('fill', self.x, self.y, self.width, self.height)
+end    
