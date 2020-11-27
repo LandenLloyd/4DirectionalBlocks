0dc4fad64091cf848a2615ce0f7c5b5e62edc0bf
klwieland <73842843+klwieland@users.noreply.github.com>
2020-11-27 16:31:09 -0500

    Update CenterBlock.lua

--- CenterBlock.lua
+++ CenterBlock.lua
@@ -1,24 +1,40 @@
-CenterBlock = Class{}
+ CenterBlock = Class{}
 
 
-function CenterBlock:init(x,y,height,width)
+function CenterBlock:init(util, centerBlockTable, tetriminoTable,x,y,blockHeight,blockWidth)
     self.x = x
     self.y = y
-    self.height = height
-    self.width = height
+    self.height = blockHeight
+    self.width = blockWidth
     self.dy = 0
     self.dx = 0
 end
 
-function CenterBlock:update(dt)
-    self.y = self.y + self.dy * dt
-    self.x = self.x + self.dx * dt
+function CenterBlock:update()
+    self.y = self.y + self.dy 
+    self.x = self.x + self.dx 
+end
+
+function CenterBlock:up()
+    self.dy = self.dy - self.height
+end
+
+function CenterBlock:down()
+    self.dy = self.dy + self.height
+end 
+
+function CenterBlock:left()
+    self.dx = self.dx - self.width
+end
+
+function CenterBlock:left()
+    self.dx = self.dx + self.width
 end
 
 function CenterBlock:rotate()
-
+    centerBlockTable = util:rotate(centerBlockTable)
 end 
 
 function CenterBlock:render()
     love.graphics.square('fill', self.x, self.y, self.width, self.height)
-end    
+end 
