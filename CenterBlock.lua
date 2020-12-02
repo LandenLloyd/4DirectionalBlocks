198319fccde2adf13202c3b045ed0fe4afe75b1a
klwieland <73842843+klwieland@users.noreply.github.com>
2020-12-02 14:23:12 -0500

    Updated center block to use table

--- CenterBlock.lua
+++ CenterBlock.lua
@@ -1,46 +1,94 @@
 CenterBlock = Class{}
 
 
-function CenterBlock:init(util, centerBlockTable, tetriminoTable, x, y, width, height)
-    self.x = x
-    self.y = y
-    self.width = width
-    self.height = height
-    self.dy = 0
-    self.dx = 0
-    VIRTUAL_WIDTH = 320
-    VIRTUAL_HEIGHT = 180
+function CenterBlock:init(util, centerBlockTable, tetriminoTable, tetriminoManager, BLOCK_DIMENSION)
+    x = util:tocords(VIRTUAL_WIDTH/2, VIRTUAL_HEIGHT/2)
+    centerBlockTable[x] = {1,1,1,1}
+    self.width = BLOCK_DIMENSION
+    self.height = BLOCK_DIMENSION
 end
 
-function CenterBlock:update(dt)
-    -- moves center block along with keys
-    self.y = self.y + self.dy * dt
-    self.x = self.x + self.dx * dt
+function CenterBlock:update()   
+
 end    
+
+function CenterBlock:up()
+    for D1, color in pairs(centerBlockTable) do
+        D2 = util:parseCoords(D1)
+        D2.y = D2.y - BLOCK_DIMENSION
+        y = util:tocords(D2.x, D2.y)
+        centerBlockTable[D1] = nil
+        centerBlockTable[y] = color
+    end
+end
+
+function CenterBlock:down()
+    for D1, color in pairs(centerBlockTable) do
+        D2 = util:parseCoords(D1)
+        D2.y = D2.y + BLOCK_DIMENSION
+        y = util:tocords(D2.x, D2.y)
+        centerBlockTable[D1] = nil
+        centerBlockTable[y] = color
+    end
+end
+
+function CenterBlock:left()
+    for D1, color in pairs(centerBlockTable) do
+        D2 = util:parseCoords(D1)
+        D2.x = D2.x - BLOCK_DIMENSION
+        x = util:tocords(D2.x, D2.y)
+        centerBlockTable[D1] = nil
+        centerBlockTable[x] = color
+    end
+end
+
+function CenterBlock:right()
+    for D1, color in pairs(centerBlockTable) do
+        D2 = util:parseCoords(D1)
+        D2.x = D2.x + BLOCK_DIMENSION
+        x = util:tocords(D2.x, D2.y)
+        centerBlockTable[D1] = nil
+        centerBlockTable[x] = color
+    end
+end
+
+function CenterBlock:rotate()
+    if util:adjacentElements(tetriminoTable, centerBlockTable) then
+        centerBlockTable = util:mergeTables(tetriminoTable, centerBlockTable)
+    end
+end
 
 function CenterBlock:outOfBounds()
     -- detects if center block has gone off screen
     -- if not out of bounds returns false
-    if self.x < 0 then
-        return true
-    end
+    for D1 in pairs(centerBlockTable) do
+        D2 = util:parseCoords(D1)
+
+        if D2.x < 0 then
+            return true
+        end
     
-    if self.x > VIRTUAL_WIDTH then
-        return true
-    end
+        if D2.x > VIRTUAL_WIDTH then
+            return true
+        end
     
-    if self.y < 0 then
-        return true
-    end
+        if D2.y < 0 then
+            return true
+        end
     
-    if self.y > VIRTUAL_HEIGHT then
-        return true
-    end    
+        if D2.y > VIRTUAL_HEIGHT then
+            return true
+        end    
+    end
 
     return false
+        
 end
 
 function CenterBlock:render()
-    love.graphics.setColor(0, 128/255, 1, 1)
-    love.graphics.rectangle('fill', self.x, self.y, self.width, self.height)
-end    
+    for D1, color in pairs(centerBlockTable) do
+        D2 = util:parseCoords(D1)
+        love.graphics.setColor(color)
+        love.graphics.rectangle('fill', D2.x, D2.y, self.width, self.height)
+    end
+end 
